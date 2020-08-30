46c422c7b8e14c006a1a2aeb2aa20bb478fa6a13
khushi Agarwal <khushi.agarwal1_cs19@gla.ac.in>
2020-08-30 12:09:58 +0530

    Create the toString,equals and hashCode method for Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -6,6 +6,8 @@
  * */
 package definitions;
 
+import java.util.Objects;
+
 public class Book {
 
     private String bookName;
@@ -18,6 +20,11 @@ public class Book {
         bookISBNNumber = "1234567891234";
     }
 
+    public Book(String bookName, String authorName, String bookISBNNumber) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.bookISBNNumber = bookISBNNumber;
+    }
 
     public String getBookName() {
         return bookName;
@@ -41,5 +48,25 @@ public class Book {
 
     public void setBookISBNNumber(String bookISBNNumber) {
         this.bookISBNNumber = bookISBNNumber;
+    }
+
+    public String toString() {
+        return String.format("Book Name: %s , Author Name: %s, Book ISBN Number: %13s", getBookName(), getAuthorName(), getBookISBNNumber());
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(getBookName(), book.getBookName()) &&
+                Objects.equals(getAuthorName(), book.getAuthorName()) &&
+                Objects.equals(getBookISBNNumber(), book.getBookISBNNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBookName(), getAuthorName(), getBookISBNNumber());
     }
 }
--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -11,7 +11,7 @@ import definitions.Book;
 public class FrontDesk {
     public static void main(String[] args) {
         Book book = new Book();
-        System.out.println(book.getAuthorName());
+        System.out.println(book);
 
 
     }
